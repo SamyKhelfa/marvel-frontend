c8ce99b979f108c04789f1c088f2ad89fe0f7ac0
SamyKhelfa <60275651+SamyKhelfa@users.noreply.github.com>
2023-09-01 17:21:03 +0200

    updates files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Characters from "./components/Characters";
